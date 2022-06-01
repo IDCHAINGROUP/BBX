793297793adf54d14e2391da8683b696561ff3a5
cryptodev0755 <66730985+cryptodev0755@users.noreply.github.com>
2022-06-01 14:06:04 +0530

    v3.0.0

---
+++ src/qt/ballotbox/settings/settingswidget.h
@@ -0,0 +1,111 @@
+// Copyright (c) 2019-2020 The PIVX developers
+// Copyright (c) 2022 The Ballotbox developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef SETTINGSWIDGET_H
+#define SETTINGSWIDGET_H
+
+#include <QWidget>
+#include "qt/ballotbox/pwidget.h"
+#include "qt/ballotbox/settings/settingsbackupwallet.h"
+#include "qt/ballotbox/settings/settingsexportcsv.h"
+#include "qt/ballotbox/settings/settingsbittoolwidget.h"
+#include "qt/ballotbox/settings/settingssignmessagewidgets.h"
+#include "qt/ballotbox/settings/settingswalletrepairwidget.h"
+#include "qt/ballotbox/settings/settingswalletoptionswidget.h"
+#include "qt/ballotbox/settings/settingsmainoptionswidget.h"
+#include "qt/ballotbox/settings/settingsdisplayoptionswidget.h"
+#include "qt/ballotbox/settings/settingsmultisendwidget.h"
+#include "qt/ballotbox/settings/settingsinformationwidget.h"
+#include "qt/ballotbox/settings/settingsconsolewidget.h"
+
+class ballotboxGUI;
+
+QT_BEGIN_NAMESPACE
+class QDataWidgetMapper;
+QT_END_NAMESPACE
+
+namespace Ui {
+class SettingsWidget;
+}
+
+class SettingsWidget : public PWidget
+{
+    Q_OBJECT
+
+public:
+    explicit SettingsWidget(ballotboxGUI* parent);
+    ~SettingsWidget();
+
+    void loadClientModel() override;
+    void loadWalletModel() override;
+    void setMapper();
+    void showDebugConsole();
+    void showInformation();
+    void openNetworkMonitor();
+
+Q_SIGNALS:
+    /** Get restart command-line parameters and handle restart */
+    void handleRestart(QStringList args);
+
+private Q_SLOTS:
+    // File
+    void onFileClicked();
+    void onBackupWalletClicked();
+    void onSignMessageClicked();
+
+    // Wallet Configuration
+    void onConfigurationClicked();
+    void onBipToolClicked();
+    void onMultisendClicked();
+    void onExportCSVClicked();
+
+    // Options
+    void onOptionsClicked();
+    void onMainOptionsClicked();
+    void onWalletOptionsClicked();
+    void onDisplayOptionsClicked();
+
+    void onDiscardChanges();
+
+    // Tools
+    void onToolsClicked();
+    void onInformationClicked();
+    void onDebugConsoleClicked();
+    void onWalletRepairClicked();
+
+    // Help
+    void onHelpClicked();
+    void onAboutClicked();
+    void onResetAction();
+    void onSaveOptionsClicked();
+
+private:
+    Ui::SettingsWidget *ui;
+    int navAreaBaseHeight{0};
+
+    SettingsBackupWallet *settingsBackupWallet;
+    SettingsExportCSV *settingsExportCsvWidget;
+    SettingsBitToolWidget *settingsBitToolWidget;
+    SettingsSignMessageWidgets *settingsSingMessageWidgets;
+    SettingsWalletRepairWidget *settingsWalletRepairWidget;
+    SettingsWalletOptionsWidget *settingsWalletOptionsWidget;
+    SettingsMainOptionsWidget *settingsMainOptionsWidget;
+    SettingsDisplayOptionsWidget *settingsDisplayOptionsWidget;
+    SettingsMultisendWidget *settingsMultisendWidget;
+    SettingsInformationWidget *settingsInformationWidget;
+    SettingsConsoleWidget *settingsConsoleWidget;
+
+    QDataWidgetMapper* mapper;
+
+    QList<QPushButton*> options;
+    // Map of: menu button -> sub menu items
+    QMap <QPushButton*, QWidget*> menus;
+
+    void selectOption(QPushButton* option);
+    bool openStandardDialog(const QString& title = "", const QString& body = "", const QString& okBtn = "OK", const QString& cancelBtn = "");
+    void selectMenu(QPushButton* btn);
+};
+
+#endif // SETTINGSWIDGET_H
